b02de13f9f26b7322c3286b5819bcefcce5bd845
Mike <mdevoldere@a.a>
2022-01-18 14:51:07 +0100

    asp début démo

---
+++ asp/AspIntro2105/AspIntro2105/Controllers/TestController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspIntro2105.Controllers
+{
+    public class TestController : Controller
+    {
+        public string Index()
+        {
+            return "Hello World";
+        }
+
+        public string Toto()
+        {
+            return "<h1>Toto</h1>";
+        }
+
+        public IActionResult Barry()
+        {
+            return View();
+        }
+
+        public IActionResult Oliver()
+        {
+            ViewData["name"] = "Olivier";
+            ViewBag.master = "Jocelyn";
+            
+            return View("Pascal");
+
+        }
+    }
+}
---
+++ asp/AspIntro2105/AspIntro2105/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AspIntro2105.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
