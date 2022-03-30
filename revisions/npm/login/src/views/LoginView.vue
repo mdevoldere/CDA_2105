72b5d64f587d80008efa91952915d8ac5064dec7
Mike <mdevoldere@a.a>
2022-03-30 15:59:15 +0200

    revision_1

---
+++ revisions/npm/login/src/views/LoginView.vue
@@ -0,0 +1,40 @@
+<template>
+    <form>
+        <h2>{{ formTitle }}</h2>
+        <label for="uname">Username</label>
+        <input id="uname" type="text" placeholder="Username"> 
+        <label for="upass">Password</label>
+        <input id="upass" type="password" placeholder="Password"> 
+        <button>Login</button> 
+        <p>Forgot password ?</p>
+    </form>
+</template>
+
+<style>
+form {
+    display: flex;
+    flex-direction: column;
+    width: 50%;
+    margin: auto;
+    justify-content: center;
+    align-items: center;
+    gap: 1rem;
+}
+</style>
+
+<script>
+export default {
+    data() {
+        return {
+            formTitle: "Type your credentials"
+        }
+    }, 
+    computed: {
+    },
+    methods: {
+
+    }
+
+
+}
+</script>
